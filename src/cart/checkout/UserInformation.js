5d1b4f371d43decef23732286452bc5590d26622
Mohd Shohaib <mohdshohaibchouhan@gmail.com>
2024-07-25 12:43:19 +0530

    Quentity

--- src/cart/checkout/UserInformation.js
+++ src/cart/checkout/UserInformation.js
@@ -13,6 +13,9 @@ const CheckoutPage = () => {
   const { authData, setIsPaymentSuccessful, login, isAuthenticated, } =
     useAuth();
   const { cart, totalPrice, clearCart } = useCart();
+  const TotalOrderQeuntity=cart.reduce((total, item) => {
+    return total +  item.product_qty;
+  }, 0);
   const navigate = useNavigate();
   const { id, saasId, storeId,mobileNumber ,name} = authData;
   const [billingAddress, setBillingAddress] = useState(false);
@@ -168,6 +171,7 @@ const CheckoutPage = () => {
         order_discount: 0,
         status: "pending",
         payment_type: "COD",
+        order_qty:TotalOrderQeuntity,
         razorpay_order_id: paymentResponse.razorpay_order_id,
         razorpay_payment_id: paymentResponse.razorpay_payment_id,
         order_date: new Date(),
